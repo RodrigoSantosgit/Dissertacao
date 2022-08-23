85968ffcb7574f889ed2724e3ea1307bba9dbe93
Rodrigo Santos <56035598+RodrigoSantosgit@users.noreply.github.com>
2022-08-23 11:08:58 +0100

    Client - Server comms fixed

--- Server/server.py
+++ Server/server.py
@@ -1,4 +1,7 @@
 ''' 
+	Rodrigo Santos, n mec 89180
+	
+	- Dissertation Project
 	SERVER ACTING AS CLIENT FOR P4SWITCH UDP
 '''
 
@@ -15,37 +18,47 @@ def main():
 	time.sleep(10)
 	
 	host = 'localhost' #default
-	remoteClientAddr = "10.13.0.3" # default
-	port = "5001" # default
+	name = '' # default
+	image = '' # default
+	max_flows = '' # default
+	min_flows = '' # default
+	expMangFunc = '10.33.0.50' # default
+	serv_port = 5000 # default
+
+	## ARGUMENT CHECKING -------
 	for i in range(1, len(sys.argv),2):
 	    if (sys.argv[i] == "--host" or sys.argv[i] == "-h") and i != len(sys.argv) - 1:
 	        host = sys.argv[i + 1]
 	    elif (sys.argv[i] == "--port" or sys.argv[i] == "-p") and i != len(sys.argv) - 1:
-	        port = int(sys.argv[i + 1])
-	    elif (sys.argv[i] == "--remoteClientAddr" or sys.argv[i] == "-rmaddr") and i != len(sys.argv) - 1:
-	        remoteClientAddr = sys.argv[i + 1]
+	        serv_port = int(sys.argv[i + 1])
 	    elif (sys.argv[i] == "--mode" or sys.argv[i] == "-m") and i != len(sys.argv) - 1:
 	        mode = sys.argv[i + 1]
-	        if mode != "rightaway" and mode != "trigger":
-	            print("[ERROR] Unknown Mode of operation: " + mode +"\n Modes available are: rightaway trigger\n")
+	        if mode != "rightaway" and mode != "triggerbased":
+	            print("[ERROR] Unknown Mode of operation: " + mode +"\n Modes available are: rightaway triggerbased\n")
 	            quit()
+	    elif (sys.argv[i] == "--expMangFunc" or sys.argv[i] == "-emf") and i != len(sys.argv) - 1:
+	        expMangFunc = sys.argv[i + 1]
+	    elif (sys.argv[i] == "--name" or sys.argv[i] == "-n") and i != len(sys.argv) - 1:
+	        name = sys.argv[i + 1]
+	    elif (sys.argv[i] == "--image" or sys.argv[i] == "-i") and i != len(sys.argv) - 1:
+	        image = sys.argv[i + 1]
+	    elif (sys.argv[i] == "--maxflows") and i != len(sys.argv) - 1:
+	        max_flows = sys.argv[i + 1]
+	    elif (sys.argv[i] == "--minflows") and i != len(sys.argv) - 1:
+	        min_flows = sys.argv[i + 1]
 	    else:
 	        print("[ERROR] Unknown argument: " + sys.argv[i] + "\n")
 	        quit()
-
-	# Instantiate k8s deployment
+	
+	
+	## INITIALIZATIONS -------
+	# Instantiate k8s deployment rightaway
 	if mode == "rightaway":
-	    response = requests.put("http://10.30.0.50:8000/instantiateService?namespace=default&name=server-udp&app=server-udp&container_name=server-udp&image=server-clientlike-udp%3Alatest")
-	    print(response)
-	    #response = requests.put("http://10.16.3.20:8000/create-service?namespace=default&name=server-udp&protocol=UDP&port=5000&targetPort=44000")
-	    #print(response)
-	    
-	# Show deployement on the k8s cluster
-	#response = requests.get("http://10.4.0.20:8000/get-pods")
-	#print(response)
-
-	# take the server name and port name
-	serv_port = 5000
+	    response = requests.put("http://" + expMangFunc + ":8000/instantiateService?namespace=default&name="+name+"&app="+name+"&container_name="+name+"&image="+image)
+	# Instantiate Trigger for service instantiate
+	if mode == "triggerbased":
+	    response = requests.put("http://"+expMangFunc+":8000/instantiateTriggerBasedService?namespace=default&name="+name+"&app="+name+"&container_name="+name+"&image="+image+"&max_flows="+max_flows+"&min_flows="+min_flows)
+	
 
 	# create a socket at server side
 	# using UDP / IP protocol
@@ -55,66 +68,62 @@ def main():
 	# bind the socket with server
 	# and port number
 	conn.bind((host, serv_port))
-
-	# allow maximum 1 connection to
-	# the socket
-	#conn.listen(1)
-
-	# wait till a client accept
-	# connection
-	#conn, addr = conn.accept()
-
-	# display client address
-	#print("CONNECTION FROM:", str(addr))
-
-	# connect it to server and port
-	# number on local computer.
-	print("TRYING TO CONNECT TO: " + remoteClientAddr + ":" + str(port))
 	
-	ping(remoteClientAddr)
-	time.sleep(10)
+	## PROCESSING -------
+	time.sleep(2)
 
 	# send message to the client after
 	# encoding into binary string
 	msg = []
-	
-	'''conn.sendto(b"[SERVER] -> HELLO CLIENTS!", (remoteClientAddr, port))
+	log(" - UDP COMMS - ")
 
 	while True:
 		
 		while not msg:
 			msg, addr = conn.recvfrom(1024)
 
-		print(msg.decode())
-
 		if msg.decode() == "Bye Server":
-			conn.sendto(b"Bye Client", (remoteClientAddr, port))
-			break
+			conn.sendto(b"Bye Client", (addr[0], addr[1]))
+			log("Bye Client")
 		else:
-			msg_to_send = "[SERVER] -> Received: " + msg.decode()
-			conn.sendto(msg_to_send.encode(), (remoteClientAddr, port))
+			msg_to_send = "Received: " + msg.decode()
+			conn.sendto(msg_to_send.encode(), (addr[0], addr[1]))
+			log(msg_to_send)
 		
 		msg = []
 
 	# disconnect the server
-	conn.close()'''
+	conn.close()
 	
 	# Delete k8s deployment instantiated in rightaway mode
 	if mode == "rightaway":
-	    response = requests.delete("http://10.30.0.50:8000/deleteService?name=server-udp")
-	    print(response)
-	    #response = requests.get("http://10.16.3.20:8000/delete-service?name=server-udp")
-	    #print(response)
-	
+	    response = requests.delete("http://" + expMangFunc +":8000/deleteService?name="+name)
+
+
+###############################################
 def ping(remoteClientAddr):
 
 	# Option for the number of packets as a function of
 	param = '-n' if platform.system().lower()=='windows' else '-c'
 
 	# Building the command.
-	command = ['ping', param, '20', remoteClientAddr]
+	command = ['ping', param, '1', remoteClientAddr]
 
 	subprocess.call(command)
 
+
+###############################################
+#			LOG			#
+###############################################
+def log(msg):
+
+	# Building the command.
+	command = ['echo', msg]
+
+	subprocess.call(command)
+
+
+###############################################
 if __name__ == "__main__":
 	main()
+
