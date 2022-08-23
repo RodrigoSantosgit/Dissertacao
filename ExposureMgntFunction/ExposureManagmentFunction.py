f9a94ccc05ae9d524379671bae79795da837c043
Rodrigo Santos <56035598+RodrigoSantosgit@users.noreply.github.com>
2022-08-23 11:14:21 +0100

    Updated api calls, triggerbased call and fetchinfo created api

--- ExposureMgntFunction/ExposureManagmentFunction.py
+++ ExposureMgntFunction/ExposureManagmentFunction.py
@@ -6,11 +6,15 @@
 '''
 
 from fastapi import FastAPI
-from kafka import KafkaProducer
+from kafka import KafkaProducer  
+import subprocess
 
 app = FastAPI()
-global producer
 
+global producer
+global storage
+
+storage = {}
 producer = KafkaProducer(bootstrap_servers='10.0.2.15:9092')
 
 ###############################################
@@ -25,7 +29,7 @@ def instantiateService(namespace="default", name="None", app="None", container_n
 		return {"ERROR"}
 	
 	try:
-		msg = '[COMPUTATIONCONTROLLER] [INSTANTIATE] ' + namespace + ' ' + name + ' ' + app + ' ' + container_name + ' ' + image
+		msg = '[COMPUTATIONCONTROLLER] [INSTANTIATE] [RIGHTAWAY] ' + namespace + ' ' + name + ' ' + app + ' ' + container_name + ' ' + image
 		producer.send('ComputationManagment', msg.encode())
 	except:
 		print('\n[ERROR] Something went wrong!\n')
@@ -46,11 +50,61 @@ def deleteService(namespace="default", name="None"):
 		return {"ERROR"}
 
 	try:
-		msg = '[COMPUTATIONCONTROLLER] [DELETE] ' + namespace + ' ' + name
+		msg = '[COMPUTATIONCONTROLLER] [DELETE] [RIGHTAWAY] ' + namespace + ' ' + name
 		producer.send('ComputationManagment', msg.encode())
 	except:
 		print('\n[ERROR] Something went wrong!\n')
 		return {"ERROR"}
 
 	return {"SUCCESS"}
+
+
+###############################################
+#	Instantiate Service Trigger Based	#
+###############################################
+@app.put("/instantiateTriggerBasedService")
+def instantiateTriggerBasedService(namespace="default", name="None", app="None", container_name="None", image="None", port: int = 1, max_flows: int = 0, min_flows: int = 0):
+
+	global producer
+	global storage
+
+	if name=="None" or app=="None" or container_name=="None" or image=="None" or max_flows == 0 or min_flows == 0:
+		return {"ERROR"}
+		
+	storage[port] = [namespace, name, app, container_name, image]
+
+	try:
+		msg = '[NETCONTROLLER] [INSTANTIATE] [FLOWCOUNTER] '+ str(port) + ' ' + str(max_flows) + ' ' + str(min_flows)
+		producer.send('NetManagment', msg.encode())
+	except:
+		print('\n[ERROR] Something went wrong!\n')
+		return {"ERROR"}
+
+	return {"SUCCESS"}
+	
+
+###############################################
+#	Instantiate Service Trigger Based	#
+###############################################
+@app.get("/fetchInfo")
+def fetchInfo(port: int = 0):
+
+	global storage
+
+	if port == 0:
+		return {"ERROR"}
+		
+	info = storage.get(port)
+
+	return info
+
+###############################################
+#			LOG			#
+###############################################
+def log(msg):
+
+	# Building the command.
+	command = ['echo', msg]
+
+	subprocess.call(command)
 
