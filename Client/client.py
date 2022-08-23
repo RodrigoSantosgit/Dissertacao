a14edaa43a12b2db065b058aa781e0c480ba9fae
Rodrigo Santos <56035598+RodrigoSantosgit@users.noreply.github.com>
2022-08-23 11:08:36 +0100

    Client - Server comms fixed

--- Client/client.py
+++ Client/client.py
@@ -1,4 +1,7 @@
 ''' 
+	Rodrigo Santos, n mec 89180
+	
+	- Dissertation Project
 	ECHO CLIENT UDP
 '''
 
@@ -12,15 +15,19 @@ import subprocess
 
 def main():
 
-	time.sleep(7)
+	time.sleep(13)
 
 	remoteClientAddr = "10.20.0.4" # default
-	port = "5000"     #default
+	remport = "5000"     #default
+	port = 5001 # default
+	
 	for i in range(1, len(sys.argv),2):
 	    if (sys.argv[i] == "--host" or sys.argv[i] == "-h") and i != len(sys.argv) - 1:
 	        host = sys.argv[i + 1]
 	    elif (sys.argv[i] == "--port" or sys.argv[i] == "-p") and i != len(sys.argv) - 1:
 	        port = int(sys.argv[i + 1])
+	    elif (sys.argv[i] == "--remoteport" or sys.argv[i] == "-rp") and i != len(sys.argv) - 1:
+	        remport = int(sys.argv[i + 1])
 	    elif (sys.argv[i] == "--remoteClientAddr" or sys.argv[i] == "-rmaddr") and i != len(sys.argv) - 1:
 	        remoteClientAddr = sys.argv[i + 1]
 	    else:
@@ -36,50 +43,65 @@ def main():
 	
 	# connect it to server and port
 	# number on local computer.
-	print("TRYING TO CONNECT TO: " + remoteClientAddr + ":" + str(port))
+	log("TRYING TO CONNECT TO: " + remoteClientAddr + ":" + str(remport))
 	
 	# Message exchange Cycle
-	msg = []
 	i = 0
 	
 	ping(remoteClientAddr)
 	
-	'''while True:
+	time.sleep(2)
+	log(" - UDP COMMS - \n")
+	s.sendto(b"HELLO SERVER!", (remoteClientAddr, remport))
+	log("HELLO SERVER!")
+	
+	while True:
+	    
+	    msg = []
 	    
 	    while not msg:
 	    	msg, addr = s.recvfrom(1024)
-
-	    print(msg.decode())
 
 	    if msg.decode() == "Bye Client":
 	    	break
 
 	    if i == 9:
-	    	msg = []
 	    	time.sleep(2)
 	    	send_msg = "Bye Server"
-	    	s.sendto(send_msg.encode(), (remoteClientAddr, port))
+	    	s.sendto(send_msg.encode(), (remoteClientAddr, remport))
+	    	log(send_msg)
 	    else:
-	       msg = []
-
 	       time.sleep(2)
 	       send_msg = "MSG Number = " + str(i)
-	       s.sendto(send_msg.encode(), (remoteClientAddr, port))
+	       s.sendto(send_msg.encode(), (remoteClientAddr, remport))
+	       log(send_msg)
 	       i = i + 1
 
 	# disconnect the client and remove file
-	s.close()'''
-	
+	s.close()
+
+#############################################
 def ping(remoteClientAddr):
 
 	# Option for the number of packets as a function of
 	param = '-n' if platform.system().lower()=='windows' else '-c'
 
 	# Building the command.
-	command = ['ping', param, '20', remoteClientAddr]
+	command = ['ping', param, '1', remoteClientAddr]
 
 	subprocess.call(command)
 
+###############################################
+#			LOG			#
+###############################################
+def log(msg):
+
+	# Building the command.
+	command = ['echo', msg]
+
+	subprocess.call(command)
+
+###############################################
 
 if __name__ == "__main__":
 	main()
